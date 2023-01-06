a52a01a4507b8a35b1c3c632d4d89234b92aef92
devmdtanvir <officialtanvir@hotmail.com>
2023-01-06 20:53:43 +0600

    I am connect to mongodb server and send data

---
+++ __staps.js
@@ -0,0 +1,32 @@
+/*
+*1 Create node server with 5 stapes 
+*1.1 Create folder 
+*1.2 npm init ----> initialize
+*1.3 npm i express mongodb cors 
+*1.4 script > nodmone index.js
+*1.5 Create index.js file
+*1.6 Usease 5 stapes to create a node server
+*
+*
+*---------------------------------------------------
+* Create Atlas Account 
+*----------------------------------------------------
+*
+*1. Sign up Google Authentication
+*2. Create Cluster 
+*3. Create  User db and password
+*4. Network Access ---> ip address allow all
+*5. database >connect > code coppy and past index.js 
+*--------------------------------------------------
+*CRUD Oparetion 
+*1. node mongodb CRUD > Fundamantles
+*2. Create async run function
+*----------------------------------
+* Intrigate client to data send server
+*1. Client side form create  
+*2. onSubmit Eventhandlar send data to server 
+*3. on Server : Create user POST To recived data to the backend 
+
+
+
+*/
--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const cors = require('cors');
-const port = 3000;
+const port = 3001;
 
 app.use(cors());
+app.use(express.json());
 
 //userName : admin1
 // password : KcpagiArOAa8SKBR
@@ -18,9 +19,17 @@ async function run() {
     try {
         await client.connect()
         const userCollaetion = client.db("foodExpress").collection('user');
-        const user = { name: 'Tanvir', email: 'info@gmail.com' };
-        const result = await userCollaetion.insertOne(user);
-        console.log(`User instrated with id ${result.insertedId}`);
+        // const user = { name: 'Tanvir', email: 'info@gmail.com' };
+        // const result = await userCollaetion.insertOne(user);
+        // console.log(`User instrated with id ${result.insertedId}`);
+
+        app.post('/user', async (req, res) => {
+            const user = req.body;
+            console.log(user, 'User recived');
+            const result = await userCollaetion.insertOne(user);
+            console.log(result.insertedId)
+            res.send(result);
+        })
     }
     finally {
         // await client.close();
@@ -30,10 +39,6 @@ async function run() {
 
 app.get('/', (req, res) => {
     res.send('My Mongodb server is ready')
-})
-
-app.post('/addUser', (req, res) => {
-    res.send('Post request click')
 })
 
 app.listen(port, () => console.log(`listinign port ${port}`))
